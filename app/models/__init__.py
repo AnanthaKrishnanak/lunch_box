a97a86417e4449c7eb6732731b642eb97c11b969
Anantha Krishnan <ananthakrishnanks54@gmail.com>
2026-07-19 19:41:37 +0530

    feat(system_settings): add system_settings model & crud operations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,7 @@
+from app.models.system_settings import SystemSettings
 from app.models.user import User
 
 __all__ = [
     "User",
+    "SystemSettings",
 ]
---
+++ app/services/system_settings.py
@@ -0,0 +1,34 @@
+from app.models.system_settings import SystemSettings
+from app.repositories.system_settings import SystemSettingsRepository
+from app.schemas.system_settings import SystemSettingsBase
+
+
+class SystemSettingsService:
+    def __init__(self, system_settings_repository: SystemSettingsRepository):
+        self.repository = system_settings_repository
+
+    async def get(self) -> SystemSettings | None:
+        return await self.repository.get()
+
+    async def create(self, payload: SystemSettingsBase) -> SystemSettings:
+        system_settings = SystemSettings(**payload.model_dump())
+        return await self.repository.create(system_settings)
+
+    async def update(self, payload: SystemSettingsBase) -> SystemSettings:
+        system_settings = await self.repository.get()
+
+        if system_settings is None:
+            system_settings = await self.create(payload)
+
+        update_data = payload.model_dump(exclude_unset=True)
+
+        for field, value in update_data.items():
+            setattr(system_settings, field, value)
+
+        return await self.repository.update(system_settings)
+
+    async def delete(self) -> None:
+        system_settings = await self.repository.get()
+        if system_settings is None:
+            return None
+        await self.repository.delete(system_settings)
